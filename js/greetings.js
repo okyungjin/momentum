196d42cc3f4d880585d73dce9efdd97fa883dc34
okyungjin <okyungjin@gmail.com>
2022-10-06 23:38:11 +0900

    ✨ feat: add greetings after login

---
+++ js/greetings.js
@@ -0,0 +1,32 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+const onLoginSubmit = (event) => {
+  console.log(1)
+  event.preventDefault(); // prevent submitting form
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  hideLoginForm();
+  showGreetings(username);
+};
+
+const hideLoginForm = () => {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+};
+
+const showGreetings = (username) => {
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+};
+
+const storedUsername = localStorage.getItem(USERNAME_KEY);
+if (storedUsername) {
+  hideLoginForm();
+  showGreetings(storedUsername);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
