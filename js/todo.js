3d5a73d3384ec9f8d20fa1430c0798b668deba6d
okyungjin <okyungjin@gmail.com>
2022-10-08 17:08:25 +0900

    ✨ feat: save todos in local storage

--- js/todo.js
+++ js/todo.js
@@ -1,19 +1,32 @@
 const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-form input');
 const todoList = document.querySelector('#todo-list');
-
 todoForm.addEventListener('submit', handleTodoSubmit);
+
+const TODOS_KEY = 'todos';
+let todos = getTodosInLocalStorage();
+todos.forEach(paintTodo);
 
 function handleTodoSubmit(event) {
   event.preventDefault();
-  createNewTodo(todoInput.value);
+  const newTodo = createTodoByLabel(todoInput.value);
+  paintTodo(newTodo);
+  addTodo(newTodo);
   clearTodoInput();
 }
 
-function createNewTodo(newTodo) {
+function createTodoByLabel(label) {
+  return { id: Date.now(), label };
+}
+
+function paintTodo(newTodo) {
   const li = document.createElement('li');
+  li.id = newTodo.id;
+
   const span = document.createElement('span');
-  span.innerText = newTodo;
+  span.innerText = newTodo.label;
+
+  console.log(span.innerText)
 
   const deleteBtn = document.createElement('button');
   deleteBtn.innerText = '❌';
@@ -30,5 +43,18 @@ function clearTodoInput() {
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
+  const todoIdToDelete = parseInt(li.id);
   li.remove();
+  todos = todos.filter(todo => todo.id !== todoIdToDelete);
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+function addTodo(newTodo) {
+  todos.push(newTodo);
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+function getTodosInLocalStorage() {
+  const savedTodos = localStorage.getItem(TODOS_KEY);
+  return savedTodos ? JSON.parse(savedTodos) : [];
 }
