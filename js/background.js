3b8b920926dffb23c2b6e29fc8529766626dc747
okyungjin <okyungjin@gmail.com>
2022-10-13 23:25:02 +0900

    ✨ feat: set background to random image

--- js/background.js
+++ js/background.js
@@ -3,12 +3,8 @@ import { getRandomInt } from './utils.js';
 const IMAGE_SRC = '/image';
 const images = ['background1.jpeg', 'background2.jpeg', 'background3.jpeg'];
 
-const backgroundImage = createRandomBackgroundImageElement();
-document.body.appendChild(backgroundImage);
+document.body.style.backgroundImage = `url('${getRandomImageUrl()}')`;
 
-function createRandomBackgroundImageElement() {
-  const chosenImage = images[getRandomInt(0, images.length)];
-  const backgroundImage = document.createElement('img');
-  backgroundImage.src = `${IMAGE_SRC}/${chosenImage}`;
-  return backgroundImage;
+function getRandomImageUrl() {
+  return `${IMAGE_SRC}/${images[getRandomInt(0, images.length)]}`;
 }
